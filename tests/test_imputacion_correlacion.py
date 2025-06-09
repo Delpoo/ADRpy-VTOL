cdc89a9a9f44b24ab879f4fd53ccf38132a02dce
Delpoo <delpinoeze@gmail.com>
2025-06-09 18:46:45 -0300

    Función de imputacion por correlacion casi lista pero no integrada al flujo principal, evaluada de manera independiete (falta corregir duplicidad en mejor modelo)

--- tests/test_imputacion_correlacion.py
+++ tests/test_imputacion_correlacion.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+# Agregar la carpeta raíz al sistema de rutas
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+
+from ADRpy.analisis.Modulos.imputacion_correlacion.imputacion_correlacion import imputacion_correlacion
 import pandas as pd
 from ADRpy.analisis.Modulos.imputacion_correlacion import imputacion_correlacion
 
@@ -7,4 +14,6 @@ def test_imputacion_correlacion_basica():
     assert not df.isna().any().any(), "Deberia imputar todos los valores faltantes"
     # Verificamos que el valor imputado para Potencia en la fila 2 sea cercano al calculo esperado
     valor = df.loc[2, 'Potencia']
-    assert round(valor, 3) == round(25.8272522839, 3)
+    assert round(valor, 3) == round(25.9691788448, 3)
+    assert 'Confianza' in reporte.columns
+print("hola")
